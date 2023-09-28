cfdc9e18c68a43c4f54f04e9728fcd5f85ae9d40
Ashok Choudhary <ashoka8769@gmail.com>
2023-09-28 14:37:31 +0530

    super commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import Applayout from "./pages/Applayout";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<Applayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Applayout.jsx
@@ -0,0 +1,5 @@
+function Applayout() {
+  return <div>App</div>;
+}
+
+export default Applayout;
