7a9a13482f2158ad61f126b34133ad87ae13b233
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-28 08:23:34 +0530

    error boundry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ErrorFallback from "./component/ErrorFallback.jsx";
 import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ErrorBoundary>
+    <React.StrictMode
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>
 );
